3d0b3d7e34ebded6c99c25a5555370b6a992bab5
AfifiMac <ahmedabdallah3996@gmail.com>
2026-04-19 13:10:41 +0200

    added rvclub to blogpost footer

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -17,7 +17,7 @@ const HIDE_SHELL_PAGES = [
 const HIDE_FOOTER_NEWSLETTER_PAGES = [
     'Home',
     'Blog',
-    'BlogPost',
+    // 'BlogPost',
 ];
 
 function AppLayout({ children }: { children: React.ReactNode }) {
